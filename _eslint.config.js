b1b798ea31e1fefd54298d1a5efb13834986090d
Giorgio Acosta <gioacostax@gmail.com>
2025-08-04 17:51:05 -0500

    chore: init

---
+++ _eslint.config.js
@@ -0,0 +1,24 @@
+/**
+ * This file can be used to define Eslint configuration,
+ * more info in https://eslint.org/docs/latest/use/configure/configuration-files/
+ */
+
+import { configs } from 'eslint-plugin-astro';
+import tseslint from 'typescript-eslint';
+
+/** @type {import("eslint").Linter.Config} */
+export default [
+  /**
+   * Astro config
+   * - eslint-plugin-astro
+   * - astro-eslint-parser
+   * - eslint-plugin-jsx-a11y
+   * - @typescript-eslint/parser
+   */
+  ...configs['flat/jsx-a11y-strict'],
+  {
+    files: ['**/*.astro'],
+    processor: 'astro/client-side-ts',
+    ...tseslint.configs.disableTypeChecked,
+  },
+];
